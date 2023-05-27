1b7bcacfe109d79c1431035ee58de17d54d5f0e1
nica-monica <81255854+nica-monica@users.noreply.github.com>
2023-05-27 18:38:56 +0200

    Update choropleth_maps.py

--- choropleth_maps.py
+++ choropleth_maps.py
@@ -34,6 +34,7 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUMEN],
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
+server=app.server
 
 app.layout = dbc.Container([
     dbc.Row([
